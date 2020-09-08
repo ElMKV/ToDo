0460289b24f172df831582502ad0ea772a206ed3
ELMKV <ilya.makoveev021199@gmail.com>
2020-09-08 17:05:55 +0500

    Добавил класс для заполнения recyclerview элементами

--- app/src/main/java/com/example/todo/Fragment1.java
+++ app/src/main/java/com/example/todo/Fragment1.java
@@ -9,8 +9,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Fragment1 extends Fragment {
+
+    private RecyclerView recyclerView;
+
 
     public Fragment1() {
     }
@@ -20,6 +25,10 @@ public class Fragment1 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_1, container, false);
         TextView textView = view.findViewById(R.id.detailsText);
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new RandomNumListAdapter(1234));
         return view;
     }
 }
