2ca04ace0f7f5484e85709c376aff1fc32364998
ELMKV <ilya.makoveev021199@gmail.com>
2020-09-08 14:12:30 +0500

    Добавил класс анимации смены фрагментов

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +22,61 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = findViewById(R.id.viewpager);
         viewPager.setAdapter(adapter); // устанавливаем адаптер
         viewPager.setCurrentItem(0); // выводим второй экран
+
+//        viewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
+//
+//
+//            @Override
+//            public void transformPage(@NonNull View v, float pos) {
+//                final float opacity = Math.abs(Math.abs(pos) - 1);
+//                v.dispatchWindowInsetsAnimationEnd();
+//            }
+//        });
+
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
     }
+
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0f);
+
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0f);
+            }
+        }
+    }
+
+
 
     public static class MyAdapter extends FragmentPagerAdapter {
 
@@ -42,8 +97,6 @@ public class MainActivity extends AppCompatActivity {
                     return new Fragment1();
                 case 1:
                     return new Fragment2();
-
-
                 default:
                     return new Fragment1();
             }
