9cae1d983e8e6be79f8278166dfdb96892dac816
ELMKV <ilya.makoveev021199@gmail.com>
2020-09-08 14:01:46 +0500

    Создал адаптер для view page и добавил case для перелистывания

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.todo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyAdapter adapter = new MyAdapter(getSupportFragmentManager());
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter); // устанавливаем адаптер
+        viewPager.setCurrentItem(0); // выводим второй экран
+    }
+
+    public static class MyAdapter extends FragmentPagerAdapter {
+
+        MyAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new Fragment1();
+                case 1:
+                    return new Fragment2();
+
+
+                default:
+                    return new Fragment1();
+            }
+        }
     }
 }
